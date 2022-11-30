fc4441feb6e67ecda3f58b9ac572c86d1cb01c34
badrjouad <jouad324@gmail.com>
2022-11-30 19:42:33 +0000

    REST API POST File CV

--- api/resumeParserApi.py
+++ api/resumeParserApi.py
@@ -1,23 +1,40 @@
-from flask import Flask
-from RCSS.rcss import rcssSkillRecognition
+import os
+import urllib.request
+from app import app
+from flask import Flask, request, redirect, jsonify
+from werkzeug.utils import secure_filename
 
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
-app = Flask(__name__)
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# skills = getSkillsList() (Liste des compétences extraites du CV du candidat venant de la fonction dans le fichier resume_parser d'Anne & Nicolas)
-skills = ["sql", "C++", "Postman", "c", "C#", "Gestion documents", "CDP", "Corea"]
+@app.route('/', methods=['GET'])
+def test():
+    return "Bonjour Sopra"
 
-# Test de la fonction avec une liste de compétences aléatoire
-rcssSkillRecognition(skills=skills)
+@app.route('/file-upload', methods=['POST'])
+def upload_file():
+	# check if the post request has the file part
+	if 'file' not in request.files:
+		resp = jsonify({'message' : 'No file part in the request'})
+		resp.status_code = 400
+		return resp
+	file = request.files['file']
+	if file.filename == '':
+		resp = jsonify({'message' : 'No file selected for uploading'})
+		resp.status_code = 400
+		return resp
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		resp = jsonify({'message' : 'File successfully uploaded'})
+		resp.status_code = 201
+		return resp
+	else:
+		resp = jsonify({'message' : 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
+		resp.status_code = 400
+		return resp
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-
-@app.route("/resume_parse")
-def resume_parser():
-    return
-
-if __name__ == '__main__':
-    app.run()   
+if __name__ == "__main__":
+    app.run()
