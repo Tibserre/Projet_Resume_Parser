73c68a8b63e7fc0b260740ef8d9aca58225415ea
Tibserre <thibault.serre@outlook.com>
2023-01-05 12:17:12 +0100

    JUson bien récup

--- Front/uploadPage/upload.js
+++ Front/uploadPage/upload.js
@@ -1,4 +1,33 @@
 window.onload = function () {
-    localJson = JSON.parse(localStorage.getItem('resumes_parsed'));
-    console.log(localJson);
+    JSONResponse=getDataFromAPI();
+    
 };
+
+function getResult(){
+    console.log("inside getResult");
+    fetch("http://127.0.0.1:2000/resume-parser")
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+    
+}
+
+function getDataFromAPI() {
+    return fetch('http://127.0.0.1:2000/resume-parser', {
+        method: 'GET',
+      })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        var resultat = JSON.stringify(data);
+        var resultatJSON = JSON.parse(resultat)
+        console.log(resultatJSON);
+        return resultatJSON;
+      })
+  }
+
+
+function parsingJSON(JSON){
+console.log(JSON.Promise);
+}
