f67a7444389535d077058130ebbbcc423be67ec2
Tibserre <thibault.serre@outlook.com>
2023-01-15 22:10:23 +0100

    correciton

--- Front/indexPage/index.js
+++ Front/indexPage/index.js
@@ -119,9 +119,7 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
           .then(result => {
               console.log(result);
               localStorage.setItem("resumes_parsed", JSON.stringify(result));
-              //window.open("/Front/uploadPage/upload.html");
-              window.location.href = "/Front/uploadPage/upload.html";
-              //window.location.replace("/Front/uploadPage/upload.html");
+              window.location.href = "/Front/uploadPage/upload.html";   
           })
           .catch(error => console.log('error', error));
 
